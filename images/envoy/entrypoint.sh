78bbe84da7c89737090e94d224ac9fb62aa9eb0b
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-09-23 12:05:42 +0200

    Fixes to the envoy image
    * do not run as root
    * do not include the now-defunct --v2-config-only startup option
    
    Change-Id: I641b147fd0dffcb32c2495671c47273415e794cb

--- images/envoy/entrypoint.sh
+++ images/envoy/entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 # Create the envoy config file from the env variables injected
 cd /etc && envsubst < /etc/envoy/envoy.yaml.tpl > /etc/envoy/envoy.yaml
-/usr/bin/envoy -c /etc/envoy/envoy.yaml --service-cluster "$SERVICE_NAME" --service-zone "$SERVICE_ZONE" --v2-config-only
+/usr/bin/envoy -c /etc/envoy/envoy.yaml --service-cluster "$SERVICE_NAME" --service-zone "$SERVICE_ZONE"
