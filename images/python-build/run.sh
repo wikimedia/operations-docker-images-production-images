0e8662f4867b8a9758a3d2d41900bda62a515945
Antoine Musso <hashar@free.fr>
2020-03-16 22:04:05 +0100

    Add an image for python2 app based on Buster
    Zuul runs on Jessie and we need to upgrade the host to Buster. It is a
    python 2 application and we need a build image. Our time frame is too
    limited to attempt completing the python 3.
    
    Add a PYTHON_SUFFIX variable to the common run.sh entry point to avoid
    copy pasting it. It defaults to '3' and is set to '2' for this image.
    
    Bug: T215458
    Change-Id: Ie6d5988b2404ebb175a7ebdb224516934b98c88d

--- images/python-build/run.sh
+++ images/python-build/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+PYTHON_SUFFIX=${PYTHON_SUFFIX:-3}
+PIP=/usr/bin/pip"$PYTHON_SUFFIX"
+PYTHON=python"$PYTHON_SUFFIX"
+
 set -eu
 
-/usr/bin/pip3 wheel -r /deploy/frozen-requirements.txt -w /wheels
+$PIP wheel -r /deploy/frozen-requirements.txt -w /wheels
 
 if [[ -e "/deploy/src/setup.py" ]]; then
-    (cp -ax /deploy /tmp/ && cd /tmp/deploy/src && python3 setup.py bdist_wheel)
+    (cp -ax /deploy /tmp/ && cd /tmp/deploy/src && $PYTHON setup.py bdist_wheel)
     mv /tmp/deploy/src/dist/*.whl /wheels/
 else
     echo "No src/setup.py found, building a dependency-only package"
