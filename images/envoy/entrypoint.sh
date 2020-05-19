37e917c7d60d718d9c3a580de3ff24019650ad14
Janis Meybohm <jmeybohm@wikimedia.org>
2020-05-19 18:19:03 +0200

    envoy: Don't try to create a envoy config if it already exists
    This is needed to use the container with envoy config configmap: Ie7f4c37508866de05e96eea104c8b8f6fa1e3454
    
    Bug: T244843
    
    Change-Id: I854a981d816a8e1cbda3218295ecf6eada9d5096

--- images/envoy/entrypoint.sh
+++ images/envoy/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-# Create the envoy config file from the env variables injected
-cd /etc && envsubst < /etc/envoy/envoy.yaml.tpl > /etc/envoy/envoy.yaml
-/usr/bin/envoy -c /etc/envoy/envoy.yaml --service-cluster "$SERVICE_NAME" --service-zone "$SERVICE_ZONE"
+# If no envoy config files exist, create one based on the template and env variables injected
+if [ ! -e /etc/envoy/envoy.yaml ]; then
+    cd /etc && envsubst < /etc/envoy/envoy.yaml.tpl > /etc/envoy/envoy.yaml
+fi
+exec /usr/bin/envoy -c /etc/envoy/envoy.yaml --service-cluster "$SERVICE_NAME" --service-zone "$SERVICE_ZONE"
