db11de0b4afe96f7ddc599ebdc6965ccc1265a23
Janis Meybohm <jmeybohm@wikimedia.org>
2020-06-29 11:49:00 +0200

    Remove deprecated and unmaintained image: envoy-tls-local-proxy
    envoy-tls-local-proxy has been deprecated by the generic envoy image
    (production-images/images/envoy).
    
    Bug: T253396
    Change-Id: I656cd1cc7ba6f5a597fc845a9d90cdd34cce289d

--- images/envoy-tls-local-proxy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-# Set optional variables to defaults if nothing is declared.
-ADMIN_LISTEN=${ADMIN_LISTEN:-127.0.0.1}
-ADMIN_PORT=${ADMIN_PORT:-1667}
-UPSTREAM_TIMEOUT=${UPSTREAM_TIMEOUT:-60.0s}
-# Create the envoy config file from the env variables injected
-envsubst < /etc/envoy/envoy.yaml.tpl > /etc/envoy/envoy.yaml
-exec /usr/bin/envoy -c /etc/envoy/envoy.yaml --service-cluster "$SERVICE_NAME" --service-zone "$SERVICE_ZONE"
