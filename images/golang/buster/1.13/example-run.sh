9f780c2d55d1cd11f563a22281113ac473427150
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2021-05-18 17:45:45 +0200

    New golang version in the standard image
    * Move to go 1.14, needed to build knative, still in backports
    * Remove the "1.13" directory, as there is really no reason to keep it
    
    Change-Id: I540d449bcbaffce6d059e991b71f851228ab4ae5

--- images/golang/buster/1.13/example-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-EXAMPLE="github.com/golang/example/outyet"
-docker run \
-    --volume /"$(pwd)"/log://log \
-    docker-registry.wikimedia.org/golang:1.13-1 /bin/bash -c "go get $EXAMPLE && cd src/$EXAMPLE && go build && echo OK"
