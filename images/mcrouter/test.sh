850f6bd00a375923822dc64437e369d0b8062925
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2021-06-01 08:19:50 +0200

    Fix tests for mcrouter
    Change-Id: Ia0b2c2d38b6feec0302412d70153d5475c087307

--- images/mcrouter/test.sh
+++ images/mcrouter/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is why we should never use bash for scripts. Ever.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-docker run -d -v $DIR/test/config:/etc/mcrouter:ro --name test-mcrouter --rm docker-registry.wikimedia.org/mcrouter
+docker run -d -v $DIR/test/config:/etc/mcrouter:ro --name test-mcrouter --rm $1
 docker exec test-mcrouter /bin/healthz
 RETVAL=$?
 docker stop test-mcrouter
