439f529469266082e0f2240a70d0e0a5a4df8e78
Luca Toscano <ltoscano@wikimedia.org>
2023-04-18 10:58:36 +0200

    amd-gpu-tester: improve run-test.sh and add nobody to the 'render' group
    Bug: T333009
    Change-Id: Ia9894785570f5b9e07cd68e05b63c5a35613e9bc

--- images/amd/gpu-tester/run-tests.sh
+++ images/amd/gpu-tester/run-tests.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+set -x
+
+# Check the kfd device and its permissions
+ls -l /dev/kfd
+
+# Check rocminfo
+/opt/rocm/bin/rocminfo
+
+# Prepare the test environment
 cd /tests
 source gpu-tests/bin/activate
+
 # First test - Alexnet (suggested by AMD upstream)
 python3 benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=alexnet
