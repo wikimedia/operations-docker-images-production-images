6bf4f8786ad2f741bd9c8fc0a0a5429560f21a6f
Antoine Musso <hashar@free.fr>
2023-07-20 16:11:08 +0200

    python-build: set date of source files in the wheels
    When updating solely the dependencies of a python project, I would do a
    diff of the wheels. bdist_wheel regenerates files with the current
    running time which do not match the time from the previous build. That
    leads to a bit of noise in the diff. An example for Zuul shows the
    following files diffing solely by timestamp:
    
        zuul-2.5.2.dev30.data/scripts/zuul-clear-refs.py
        zuul-2.5.2.dev30.dist-info/AUTHORS
        zuul-2.5.2.dev30.dist-info/LICENSE
        zuul-2.5.2.dev30.dist-info/METADATA
        zuul-2.5.2.dev30.dist-info/WHEEL
        zuul-2.5.2.dev30.dist-info/dependency_links.txt
        zuul-2.5.2.dev30.dist-info/entry_points.txt
        zuul-2.5.2.dev30.dist-info/pbr.json
        zuul-2.5.2.dev30.dist-info/top_level.txt
        zuul-2.5.2.dev30.dist-info/RECORD
    
    We can ask wheels to set the source file date by setting
    SOURCE_DATE_EPOCH environment variable. This change set it to the Unix
    epoch of the HEAD commiter date.
    
    Bug: T259611
    Bug: T342346
    Change-Id: I90cceab6b3f42d4a9660c795e8acf4285037447a

--- images/python-build/run.sh
+++ images/python-build/run.sh
@@ -9,7 +9,13 @@ set -eu
 $PIP wheel -r /deploy/frozen-requirements.txt -w /wheels
 
 if [[ -e "/deploy/src/setup.py" ]]; then
-    (cp -ax /deploy /tmp/ && cd /tmp/deploy/src && $PYTHON setup.py bdist_wheel)
+    cp -ax /deploy /tmp/
+    (
+    cd /tmp/deploy/src
+    # Setting source files to commit date of HEAD
+    SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct) \
+        $PYTHON setup.py bdist_wheel
+    )
     mv /tmp/deploy/src/dist/*.whl /wheels/
 else
     echo "No src/setup.py found, building a dependency-only package"
