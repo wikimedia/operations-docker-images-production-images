9e4fe08c17e0952216d58eb3c8457b44c8bf1598
Antoine Musso <hashar@free.fr>
2023-07-25 17:09:30 +0200

    python-build: ensure frozen-requirements is exhaustive
    `pip wheel -r requirements.txt` installs transitive dependencies and the
    generated artifact file can thus hold unwanted dependencies updates as a
    result which defeat the purpose of freezing dependencies.
    
    When building the wheels, pass `--no-deps` to prevent installation of
    transitive dependencies.
    
    To verify the set of generated wheels fullfil their requirements, run a
    second wheel build disabling the package index (`--no-index`) and only
    relying on the build wheels (`--find-links /wheels`).
    
    As a result, if a dependency is missing in `frozen-requirements.txt`,
    the build aborts immediately mentioning the missing dependency.
    
    Example:
    
    Build the wheel for `requests` without its dependencies:
    
      $ pip wheel --no-deps -w wheels requests
      Collecting requests
        Using cached requests-2.31.0-py3-none-any.whl (62 kB)
      Saved ./wheels/requests-2.31.0-py3-none-any.whl
    
    Checking the result then complains about the missing dependency
    charset-normalizer:
    
      $ pip wheel --quiet --no-index --find-links ./wheels requests
      ERROR: Could not find a version that satisfies the requirement
      charset-normalizer<4,>=2 (from requests) (from versions: none)
      ERROR: No matching distribution found for charset-normalizer<4,>=2
    
    Bug: T342346
    Change-Id: Ia2d37c2d1326c22945d8ea22faa0f9f2fda1130d

--- images/python-build/run.sh
+++ images/python-build/run.sh
@@ -6,7 +6,27 @@ PYTHON=python"$PYTHON_SUFFIX"
 
 set -eu
 
-$PIP wheel -r /deploy/frozen-requirements.txt -w /wheels
+echo "Creating wheels (without transitive dependencies)"
+$PIP wheel --no-deps -r /deploy/frozen-requirements.txt -w /wheels
+
+echo "Verifying generated wheels fullfil frozen requirements"
+verif_dir=$(mktemp -d --tmpdir python-build-verif.XXXX)
+trap 'rm -R "$verif_dir"' EXIT
+mkdir -p "$verif_dir"
+
+# --isolated prevents environment variables such as PIP_FIND_LINKS which would
+# override let pip discover extra wheels beside the one we build.
+# --no-index prevents looking up in the package index and instead pip can only
+# search the wheels we have built via --find-links /wheels
+#
+# If a dependency is missing somehow, the build will fail indicating
+# frozen-requirements.txt misses an entry.
+$PIP wheel \
+	--quiet --isolated \
+	--no-index \
+	--find-links /wheels \
+	-r /deploy/frozen-requirements.txt \
+	-w "$verif_dir"
 
 if [[ -e "/deploy/src/setup.py" ]]; then
     cp -ax /deploy /tmp/
