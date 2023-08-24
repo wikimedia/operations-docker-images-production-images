7b6f8cda2d3b0b7d024446fb74637ec692488b93
Clément Goubert <cgoubert@wikimedia.org>
2023-08-24 11:32:00 +0200

    envoy: Add concurrency control to envoy cmdline
    Add a CONCURRENCY environment variable to set envoy's --concurrency CLI
    switch. If this variable is not set, concurrency is default, which is
    the number of hardware CPUs.
    
    Bug: T344814
    Change-Id: I5d47d2fc91d4e96561ae0151c47796856e5d5de1

--- images/envoy/entrypoint.sh
+++ images/envoy/entrypoint.sh
@@ -7,10 +7,16 @@ fi
 DRAIN_TIME_S=${DRAIN_TIME_S:=600}
 DRAIN_STRATEGY=${DRAIN_STRATEGY:="gradual"}
 SERVICE_NODE=${SERVICE_NODE:=$HOSTNAME}
+if [ -n "${CONCURRENCY}" ]; then
+    CONCURRENCY_CLI_SWITCH="--concurrency ${CONCURRENCY}"
+else
+    CONCURRENCY_CLI_SWITCH=""
+fi
 
 exec /usr/bin/envoy -c /etc/envoy/envoy.yaml \
     --service-node "$SERVICE_NODE" \
     --service-cluster "$SERVICE_NAME" \
     --service-zone "$SERVICE_ZONE" \
     --drain-time-s "$DRAIN_TIME_S" \
-    --drain-strategy "$DRAIN_STRATEGY"
+    --drain-strategy "$DRAIN_STRATEGY" \
+    ${CONCURRENCY_CLI_SWITCH}
