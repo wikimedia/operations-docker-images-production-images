34d7fe6254c43cc4484393256baa2e93119dfb82
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2025-03-27 08:55:14 +0100

    Remove golang-1.17 and golang-1.18 images
    1.17 and 1.18 were only ever present in bullseye-backports.
    backports is only supported for the life time of Debian
    stable/oldstable, but not LTS and it will be archived soon.
    
    Looking at debmonitor no images currently tracked in
    Debmonitor use these anymore.
    
    Spark images were fixed in T390139
    
    knative actually uses 1.22
    
    Bug: T383557
    Change-Id: I3e56101b0942ebfdcd58ecd52d6031ad9d11cc50

--- images/golang/1.17/example-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-EXAMPLE="github.com/golang/example/outyet"
-docker run \
-    --volume /"$(pwd)"/log://log \
-    docker-registry.wikimedia.org/golang1.17:1.17-1 /bin/bash -c "go get $EXAMPLE && cd src/$EXAMPLE && go build && echo OK"
